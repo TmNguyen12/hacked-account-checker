2ac093caa1294cc12d23e15c03c4b8383af4d89c
Truong Nguyen <tmnguyen12@gmail.com>
2018-05-23 17:07:04 -0700

    Show and hide description finished

--- src/components/Results/Item.jsx
+++ src/components/Results/Item.jsx
@@ -1,52 +1,61 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import DOMPurify from 'dompurify';
 
 class Item extends Component {
-  constructor(props){
-    super(props); 
+  constructor(props) {
+    super(props);
 
-    // this.toggleDescription = this.toggleDescription.bind(this); 
+    this.toggleDescription = this.toggleDescription.bind(this);
   }
 
-  
-  // toggleDescription() {
-  //   const itemText = document.getElementsByClassName('result-text')[0];
-  //   const hiddenNode = document.getElementsByClassName('hidden'); 
-
-  //   console.log('itemText', itemText);
-  //   console.log('hiddenNode', hiddenNode); 
-  //   if (hiddenNode) {
-  //     hiddenNode.classList.remove('hidden');
-  //   } else {
-  //     hiddenNode.classList.add('hidden');
-  //   }
-  // }
-
+  toggleDescription(e) {
+    console.log('e',e);
+    let shownItem = document.getElementsByClassName('show'); 
+    console.log('shown item', shownItem); 
+    debugger 
+    if (shownItem.length > 0) {
+      shownItem[0].classList.add('hidden'); 
+      shownItem[0].classList.remove('show'); 
+    }
+    let itemText = document.getElementById(e.target.dataset.internalid);
+    itemText.classList.remove('hidden');
+    itemText.classList.add('show');  
+  }
 
   render() {
-    const { breach } = this.props; 
+    const { breach } = this.props;
     return (
-      <div className="result-item">
-        <div className="logo-container">
+      <div className="result-item" data-internalid={`${breach.Title}`} onClick={this.toggleDescription}>
+        <div className="logo-container" data-internalid={`${breach.Title}`}>
           <img
             src={`//logo.clearbit.com/${breach.Domain}`}
             className="logo"
             alt="logo"
+            data-internalid={`${breach.Title}`}
           />
         </div>
-        <div className={`result-text ${breach.Title}`} onClick={this.toggleDescription}>
-          <p className="Title">{breach.Title}</p>
-          <p className="breach-date">Breach Date: {breach.BreachDate}</p>
-          <div className="description hidden">{breach.Description}</div>
-          <p className="data-classes">
+        <div
+          className={`result-text ${breach.Title}`}
+          
+          data-internalid={`${breach.Title}`}
+        >
+          <p className="Title" data-internalid={`${breach.Title}`}>{breach.Title}</p>
+          <p className="breach-date" data-internalid={`${breach.Title}`}>Breach Date: {breach.BreachDate}</p>
+          <div
+            className="description hidden" id={`${breach.Title}`}
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(breach.Description)
+            }}
+          />
+          <p className="data-classes" data-internalid={`${breach.Title}`}>
             Compromised Data: <br /> {breach.DataClasses.join(', ')}
           </p>
         </div>
       </div>
     );
   }
-
-};
+}
 
 Item.propTypes = {
   breach: PropTypes.object
